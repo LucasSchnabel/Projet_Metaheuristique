e716c8fdd804c17dd5dc3291ea81b711a21c9527
Jean-Charles Lionnet <jclionnet@hotmail.fr>
2018-05-21 13:19:01 +0200

    Test recuit à vérifier

--- src/solution/test.java
+++ src/solution/test.java
@@ -1,6 +1,7 @@
 package solution;
 
 import algo.Greedy;
+import algo.RecuitSimule;
 
 public class test {
 
@@ -13,7 +14,7 @@ public class test {
 		Solution initiale = new Solution(nbTache,nbPers,tab);
 		Greedy g = new Greedy(initiale);
 		
-		
+		RecuitSimule r = new RecuitSimule(initiale, 1000);
 		
 		g.trouverSolution();
 		
@@ -21,6 +22,11 @@ public class test {
 		System.out.println(g.getSolutionEnCours().evaluation());
 		System.out.println();
 		while(!g.ameliorerSolution()){System.out.println(g.getSolutionEnCours());}
+		System.out.println("Recuit");
+		for (int i = 0; i < 100; i++) {
+			r.ameliorerSolution();
+		}
+		System.out.println(r.getSolutionEnCours());
 
 	}
 
